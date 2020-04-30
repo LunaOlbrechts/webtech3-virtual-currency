cd73bc45bc4ae93cfddf19be994c20e45c94252f
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-04-30 13:27:20 +0200

    Implemented primus to the leaderboard

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,15 @@ if (!localStorage.getItem("token")) {
     window.location.href = "../login/login.html";
 }
 
+/* primus live */ 
+primus = Primus.connect("http://localhost/3000", {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+      , min: 500 // Number: The minimum delay before we try reconnect.
+      , retries: 10 // Number: How many times we should try to reconnect.
+    }
+});
+
 fetch("http://localhost:3000/api/v1/leaderboard", {
     method: "get",
     'headers': {
