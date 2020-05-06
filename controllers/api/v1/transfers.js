5c53432d052263398dbd3680cef7931a9dc70bb4
tikoStudio <timothy.koenig@mail.com>
2020-05-06 16:11:48 +0200

    added exceptions for sending 0 or negative coins

--- controllers/api/v1/transfers.js
+++ controllers/api/v1/transfers.js
@@ -47,47 +47,58 @@ const create = (req, res, next) => {
                     })
                 }else {
                     if(docs2[0].balance >= transfer.amount) {
-                        if(docs.length) {
-                            transfer.save( (err, doc) => {
-                                if(err) {
-                                    res.json({
-                                        "status": "error",
-                                        "message": "Er is iets misgelopen, de coins zijn niet verstuurd",
-                                    })
-                                }
-                        
-                                if(!err) {
-                                    res.json({
-                                        "status": "success",
-                                        "message": "Coins verstuurd",
-                                        "data": {
-                                            "transaction": doc,
-                                        }
-                                    })
-                                }
-                            })
-                            let oldBalanceSender = docs2[0].balance
-                            let newBalanceSender = oldBalanceSender - transfer.amount
-                            User.findOneAndUpdate({email: transfer.sender}, {balance: newBalanceSender}, (err,docs) => {
-                                //console.log(oldBalanceSender)
-                                //console.log(newBalanceSender)
-                                //console.log(docs)
-                            })
-        
-                            let oldBalanceReceiver = docs[0].balance
-                            let newBalanceReveiver = oldBalanceReceiver + transfer.amount
-                            User.findOneAndUpdate({email: transfer.receiver}, {balance: newBalanceReveiver}, (err,docs) => {
-                                //console.log(oldBalanceReceiver)
-                                //console.log(newBalanceReveiver)
-                                //console.log(docs)
-                            })
-                
-                        }else {
+                        if(transfer.amount == 0) {
                             res.json({
                                 "status": "error",
-                                "message": "De user waar je coins naar wilde sturen is niet gevonden",
+                                "message": "je moet meer dan 0 coins per keer sturen.",
                             })
+                        }else {
+                            let stringers = transfer.amount
+                            let substring = "-"
+                            if(stringers < 0) {
+                                res.json({
+                                    "status": "error",
+                                    "message": "je kan geen negatieve bedragen sturen!",
+                                })
+                            }else {
+                                if(docs.length) {
+                                    transfer.save( (err, doc) => {
+                                        if(err) {
+                                            res.json({
+                                                "status": "error",
+                                                "message": "Er is iets misgelopen, de coins zijn niet verstuurd",
+                                            })
+                                        }
+                                
+                                        if(!err) {
+                                            res.json({
+                                                "status": "success",
+                                                "message": "Coins verstuurd",
+                                                "data": {
+                                                    "transaction": doc,
+                                                }
+                                            })
+                                        }
+                                    })
+                                    let oldBalanceSender = docs2[0].balance
+                                    let newBalanceSender = oldBalanceSender - transfer.amount
+                                    User.findOneAndUpdate({email: transfer.sender}, {balance: newBalanceSender}, (err,docs) => {
+                                    })
+                
+                                    let oldBalanceReceiver = docs[0].balance
+                                    let newBalanceReveiver = oldBalanceReceiver + transfer.amount
+                                    User.findOneAndUpdate({email: transfer.receiver}, {balance: newBalanceReveiver}, (err,docs) => {
+                                    })
+                        
+                                }else {
+                                    res.json({
+                                        "status": "error",
+                                        "message": "De user waar je coins naar wilde sturen is niet gevonden",
+                                    })
+                                }
+                            } 
                         }
+                        
                     }else {
                         res.json({
                             "status": "error",
