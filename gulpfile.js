f120d42e2f72e2b384db26c819c20f113611dc55
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-05-12 14:56:48 +0200

    Rearranged gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest } = require('gulp');
 const minify = require('gulp-minify');
 
-exports.minifyJs = function() {
+function jsMinify (){
     return src(['./frontend/js/*.js'])
-      .pipe(minify({
-        noSource: true
-      }))
-      .pipe(dest('dist/minifyjs'))
-  };
+    .pipe(minify({
+      noSource: true
+    }))
+    .pipe(dest('dist/app.js'))
+}
+
+exports.jsMinify = jsMinify;
